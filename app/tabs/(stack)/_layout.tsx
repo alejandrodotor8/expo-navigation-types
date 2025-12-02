15cb3a50cf9295a7d6f76f2c3769dc2302a501c4
Alejandro Dotor <alejandro.dotor@pragma.com.co>
2025-12-02 17:08:03 -0500

    add tabs

--- app/tabs/(stack)/_layout.tsx
+++ app/tabs/(stack)/_layout.tsx
@@ -5,7 +5,6 @@ export default function StackLayout() {
   return (
     <Stack
       screenOptions={{
-        //headerShown: false
         headerShadowVisible: false,
         headerStyle: {
           backgroundColor: "#fff",
@@ -15,8 +14,18 @@ export default function StackLayout() {
         },
       }}
     >
-      <Stack.Screen name="home/index" options={{ title: "Home" }} />
-      <Stack.Screen name="products/index" options={{ title: "Products" }} />
+      <Stack.Screen
+        name="home/index"
+        options={{ title: "Home", headerShown: false }}
+      />
+      <Stack.Screen name="products/index" options={{ title: "All products" }} />
+      <Stack.Screen
+        name="products/[id]"
+        options={{
+          title: "Product details",
+          headerStyle: { backgroundColor: "#EAD4FF" },
+        }}
+      />
       <Stack.Screen name="profile/index" options={{ title: "Profile" }} />
       <Stack.Screen name="settings/index" options={{ title: "Settings" }} />
     </Stack>
---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,37 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Tabs } from "expo-router";
+
+export default function TabsLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "indigo" }}>
+      <Tabs.Screen
+        name="home/index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="home-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="favorites/index"
+        options={{
+          title: "Favorites",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="star-outline" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="(stack)"
+        options={{
+          headerShown: false,
+          title: "Stack",
+          tabBarIcon: ({ color }) => (
+            <Ionicons size={28} name="reader-outline" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
